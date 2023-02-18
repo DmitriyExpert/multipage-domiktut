04c699c0695c01ab591850cfe1e270fbfdb50bba
--Dmitriy <dpemvr@yandex.ru>
2023-02-18 20:44:30 +0500

    The end of the development of the first version of the header. Adaptation of the header for all types of devices

---
+++ assets/js/function.js
@@ -0,0 +1,20 @@
+// ANIMATION BURGER MENU
+
+
+const headerBurgermenuBtnActivated = document.querySelector('.header__burgermenu');
+const headerburgermenuActive = document.querySelector('.header__burgermenuactive');
+const headerburgermenuClosed = document.querySelector('.burgermenuactive--iconclose');
+
+
+const burgerMenuActivation = function () {
+     headerburgermenuActive.classList.add('burgermenuactive--animation');
+};
+
+const burgerMenuClose = function () {
+     headerburgermenuActive.classList.add('burgermenuactive--animationclose');
+     setTimeout(() => {
+          headerburgermenuActive.classList.remove('burgermenuactive--animation');
+          headerburgermenuActive.classList.remove('burgermenuactive--animationclose');
+     }, 400);
+     
+};
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// ANIMATION BURGER MENU
+
+headerBurgermenuBtnActivated.addEventListener('click', function () {
+     burgerMenuActivation();
+});
+
+headerburgermenuClosed.addEventListener('click', function () {
+     burgerMenuClose();
+});
